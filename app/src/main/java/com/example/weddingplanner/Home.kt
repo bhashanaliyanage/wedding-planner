df486a1fe13ecb69d42dead12257e08744b135db
Bhashana Liyanage <witchx44@gmail.com>
2022-11-02 12:05:11 +0530

    Fragment Navigation Working

--- app/src/main/java/com/example/weddingplanner/Home.kt
+++ app/src/main/java/com/example/weddingplanner/Home.kt
@@ -1,16 +1,29 @@
 package com.example.weddingplanner
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class Home : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
+
+    }
+
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 
     override fun onCreateView(
@@ -29,5 +42,17 @@ class Home : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        hotelsCard.setOnClickListener {
+            val intent = Intent (activity, Hotels::class.java)
+            requireActivity().startActivity(intent)
+        }
+
+        cardViewBudget.setOnClickListener {
+            replaceFragment(BudgetCalc())
+        }
     }
 }
--- app/src/main/java/com/example/weddingplanner/Hotels.kt
+++ app/src/main/java/com/example/weddingplanner/Hotels.kt
@@ -1,11 +1,21 @@
 package com.example.weddingplanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_hotels.*
 
 class Hotels : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotels)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.accent)
+
+        btnBackHotels.setOnClickListener {
+            val intent = Intent (this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
